40bd7dfaca8d222f374b5ec807e70b3ca2b69b64
Akshat Biniwale <akshatbiniwale@gmail.com>
2025-04-29 00:45:49 +0530

    clean up

--- src/constants/stables.js
+++
@@ -1,7 +0,0 @@
-const UPLOAD_FOLDER_BASE_URL = `http://localhost:${process.env.REACT_APP_PORT}/uploads/`;
-
-const stables = {
-    uploadFolderBaseUrl: `${UPLOAD_FOLDER_BASE_URL}`,
-};
-
-export default stables;
